2c8c615c2da9560823d7b529843af6b5ab827215
RoCky Wu <rocky823@gmail.com>
2017-01-05 22:13:57 +1100

    Added `check-token` redirect to /login

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 import s from './app.scss'
 import Nav from '~coms/nav'
+import checkToken from './utils/check-token'
 
 module.exports = {
   name: 'App',
+  mounted() {
+    const { $router } = this
+    checkToken()
+      .then((hasToken) => {
+        if (hasToken) {
+          return
+        }
+
+        $router.push('/login')
+      })
+  },
+  computed: {
+    classes() {
+      const { $route } = this
+      return {
+        [s.isNotPage]: /^\/(login|signup)$/.test($route.path)
+      }
+    }
+  },
   render(h) {
+    const { classes } = this
+
     return (
-      <div id="app" class={s.app}>
+      <div id="app" class={[s.app, classes]}>
         <Nav />
         <div class={[s.main]}>
           <router-view />
