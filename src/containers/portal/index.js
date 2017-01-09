f5aa265700f951440632a79d1be8e0cd7e3d8d66
Kelly Su <yoyo420303@gmail.com>
2017-01-09 15:34:49 +0800

    [portal] remove host-list tab

--- src/containers/portal/index.js
+++ src/containers/portal/index.js
@@ -1,7 +1,6 @@
 // Portal container
 import { Tab } from 'vue-owl-ui'
 import HostGroups from './host-groups'
-import HostList from './host-list'
 
 const Portal = {
   name: 'Portal',
@@ -12,13 +11,9 @@ const Portal = {
       <div>
         <Tab>
           <Tab.Head slot="tabHead" isSelected={true} name="1">HostGroups</Tab.Head>
-          <Tab.Head slot="tabHead" name="2">Hosts</Tab.Head>
 
           <Tab.Content slot="tabContent" name="1">
             <HostGroups />
-          </Tab.Content>
-          <Tab.Content slot="tabContent" name="2">
-            <HostList />
           </Tab.Content>
         </Tab>
 
